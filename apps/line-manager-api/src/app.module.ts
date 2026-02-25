0008c8d4e2ecae94d0fa2c51ae191e2a3493c538
roee-cvt <roeeheily.cvt@gmail.com>
2026-02-25 15:37:05 +0200

    Updated app.modules.ts & installed nestjs-twilio

--- apps/line-manager-api/src/app.module.ts
+++ apps/line-manager-api/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TwilioModule } from 'nestjs-twilio'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV == "preview" || process.env.NODE_ENV == "production"
+    }),
+    TwilioModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (cfg: ConfigService) => {
+        const accountSid = cfg.getOrThrow("TWLIIO_ACCOUNT_SID");
+        const authToken = cfg.getOrThrow("TWILIO_AUTH_TOKEN")
+        return { accountSid, authToken }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}

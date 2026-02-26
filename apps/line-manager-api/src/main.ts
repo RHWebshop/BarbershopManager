b3ae683b9918486c63476da9fa8c57beaad31dba
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com>
2026-02-27 00:30:59 +0200

    fix: changed default backend port

--- apps/line-manager-api/src/main.ts
+++ apps/line-manager-api/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();

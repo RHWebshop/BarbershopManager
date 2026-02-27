1e0d8008cf909664e0427d0c531b520d5bad466c
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com>
2026-02-28 00:31:46 +0200

    fix: I hope this fixes the "Parsing error: No tsconfigRootDir was set, and multiple candidate TSConfigRootDirs are present"

--- apps/line-manager-api/eslint.config.mjs
+++ apps/line-manager-api/eslint.config.mjs
@@ -4,7 +4,7 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default[
   {
     ignores: ['eslint.config.mjs'],
   },
@@ -20,7 +20,7 @@ export default tseslint.config(
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: __dirname,
       },
     },
   },
@@ -29,7 +29,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
-);
+];

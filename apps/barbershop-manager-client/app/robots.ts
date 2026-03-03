d9930dc1b69702c984178ab15869e756a287381e
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com>
2026-03-03 02:38:27 +0200

    feat: split appointments from appointment creation page

--- apps/barbershop-manager-client/app/robots.ts
+++ apps/barbershop-manager-client/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 		rules: {
 			userAgent: "*",
 			allow: "/",
-			disallow: ["/private/", "/admin/", "/cart/", "/appointments/", "/login/", "/register/"],
+			disallow: ["/private/", "/admin/", "/cart/", "/appointments/", "/appointments/new", "/login/", "/register/"],
 		},
 		sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
 	};

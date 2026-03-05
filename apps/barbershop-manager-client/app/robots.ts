510495bd1c16bfffea473e0c70500596b08ba621
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com>
2026-03-05 02:00:46 +0200

    feat: missing source map in dev error

--- apps/barbershop-manager-client/app/robots.ts
+++ apps/barbershop-manager-client/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
 			allow: "/",
 			disallow: ["/private/", "/admin/", "/cart/", "/appointments/", "/appointments/new", "/login/", "/register/"],
 		},
-		sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+		sitemap: `${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/sitemap.xml`,
 	};
 }

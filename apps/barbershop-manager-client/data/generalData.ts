7d9412035376eb627c5a499aaa6cc4e7f9d43c18
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com>
2026-03-07 02:31:10 +0200

    feat: remove home route from ROUTES,

--- apps/barbershop-manager-client/data/generalData.ts
+++ apps/barbershop-manager-client/data/generalData.ts
@@ -1,10 +1,7 @@
 export const ROUTES = [
-	{ label: "בית", href: "/" },
 	{ label: "חנות", href: "/store" },
 	{ label: "קביעת תור", href: "/appointments" },
 	{ label: "צור קשר", href: "/contact" },
 ];
 
-export const STORES ={
-    
-}
+export const STORES = {};

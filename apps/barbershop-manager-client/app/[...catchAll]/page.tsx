7eb29f59a884c79ee5585a661905dd868c00abda
RaccoonishDeveloper <139078880+RaccoonishDeveloper@users.noreply.github.com>
2026-03-08 00:10:02 +0200

    chore: add TODO

--- apps/barbershop-manager-client/app/[...catchAll]/page.tsx
+++ apps/barbershop-manager-client/app/[...catchAll]/page.tsx
@@ -1,5 +1,10 @@
 import { redirect } from "next/navigation";
 
+// TODO: if user is authenticated, redirect to home page, else redirect to login page. This is a catch-all route that will catch any undefined routes and redirect accordingly.
 export default function CatchAll() {
-	redirect("/login");
+	const user = true; // Simulate user authentication status (replace with actual auth logic)
+	if (user) redirect("/");
+	else {
+		redirect("/login");
+	}
 }
